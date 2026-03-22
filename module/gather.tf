c9fdd4431962346e0765e0efef16eae2c2bbb2a5
likhith777666 <likhithbobby000@gmail.com>
2026-03-23 02:09:02 +0530

    Added OIDC provider and IAM assume role policy for EKS service accounts (IRSA)

---
+++ module/gather.tf
@@ -0,0 +1,22 @@
+
+data "tls_certificate" "eks_certificate" {
+    url = aws_eks_cluster.eks[0].identity[0].oidc[0].issuer
+}
+
+data "aws_iam_policy_document" "eks_oidc_assume_role_policy" {
+    statement {
+      effect = "Allow"
+      actions = [ "sts:AssumeRoleWithWebIdentity" ]
+
+      condition {
+        test = "StringEquals"
+        variable = "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub"
+        values = ["system:serviceaccount:default:aws-test"]
+      }
+      principals {
+        identifiers = [aws_iam_openid_connect_provider.eks_oidc.arn]
+        type = "Federated"
+      }
+    }
+    
+}
