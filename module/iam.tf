eadab1129a6db81d9950561c32b93bc4c1f9fe73
likhith <105557448+likhith777666@users.noreply.github.com>
2026-03-30 00:03:17 +0530

    Add IAM role for ALB controller with policy attachment

--- module/iam.tf
+++ module/iam.tf
@@ -105,3 +105,29 @@ resource "aws_iam_role_policy_attachment" "eks_oidc_policy_attachment" {
     policy_arn = aws_iam_policy.eks_oidc_policy.arn
     role = aws_iam_role.eks_oids.name
 }
+
+
+resource "aws_iam_role" "alb_controller" {
+  name = "eks-alb-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks_oidc.arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "alb_attach" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = "arn:aws:iam::982534381361:policy/AWSLoadBalancerControllerIAMPolicy"
+}
